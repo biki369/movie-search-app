13242abacfcd247bee0d17116d491459893c8480
biki369 <bikitirua321@gmail.com>
2025-04-25 09:01:04 +0530

    init 25:4:25

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 items-center justify-center"
     >
-      <Text>Biki Tirua</Text>
+      <Text className="text-5xl text-blue-500 font-bold">Biki Tirua</Text>
     </View>
   );
 }
