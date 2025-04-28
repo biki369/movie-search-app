49848108b5170d3cc1fbaf846e779b27e4399bda
biki369 <bikitirua321@gmail.com>
2025-04-28 12:35:04 +0530

    init 28:4:25

---
+++ app/(tabs)/demo.tsx
@@ -0,0 +1,15 @@
+// import { View, Text } from 'react-native'
+// import React from 'react'
+
+// const demo = () => {
+//   return (
+//     <View>
+//       <Text>demo</Text>
+//     </View>
+//   )
+// }
+
+// export default demo
+
+// make a (tabs) dir and put a file which one you want show on tabs options  by default bottom of the menu tab will be shown   
+// NPM RUN RESET-PROJECT This will reset the project basic files
