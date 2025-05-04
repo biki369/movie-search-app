30a0ca269affe12f36b71a3ee359a57c6bc40853
biki369 <bikitirua321@gmail.com>
2025-05-04 12:49:36 +0530

    init 4:5:25 [api init]

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
       const router = useRouter();
+
+      const {data:movies,
+         loading:moviesLoading,
+         error:moviesError
+        } = useFetch(() => fetchMovies({query:""}),false);
+
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -14,14 +23,27 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
       >
-        <Image source={icons.logo} className="w-12 h10 mt-20 mb-5 mx-auto " />
-        <View  className="flex-1 mt-5">
-           <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search Movie"
-           />
+        {
+        moviesLoading ? (
+          <ActivityIndicator color="#ab8bff" size="large" className="mt-10 self-center" />
+        ):moviesError ? (
+          <Text className="text-white text-center mt-10">{moviesError.message}</Text>
+        ):(
+          <View  className="flex-1 mt-5">
+          <SearchBar
+           onPress={() => router.push("/search")}
+           placeholder="Search Movie"
+          />
+          <>
+           <Text className="text-white text-2xl font-semibold mb-3 mt-5">Latest Movies</Text>
+             
+          </>
 
-        </View>
+       </View>
+        )}
+
+        <Image source={icons.logo} className="w-12 h10 mt-20 mb-5 mx-auto " />
+       
       </ScrollView>
     </View>
   );
